df3bd0746467d42f45fc5d47c71dc59c0a78ae51
Advait <advait.gosai@gmail.com>
2023-12-05 17:20:38 -0500

    Update Evals.py

--- Evals.py
+++ Evals.py
@@ -4,6 +4,7 @@ THIS FILE WRITTEN BY ADVAIT GOSAI
 
 from main import Model
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import pickle
 import argparse
 import numpy as np
@@ -159,23 +160,25 @@ unique_end_reasons = set()
 markers = ['o', 's', '^', 'x']
 
 for i, exp in enumerate(experiments):
-    if (i % PRINT_EVAL_STEPS == 0) or (i == len(experiments)):
-        print(f"EXPERIMENT {i+1}")
+    # Adding data to dictionary and plots for all experiments
+    unique_end_reasons.add(exp['end_reason'])
+
+    if exp['end_reason'] in end_reason_counts:
+        end_reason_counts[exp['end_reason']] += 1
+    else:
+        end_reason_counts[exp['end_reason']] = 1
+    if exp['end_reason'] not in end_reason_plots:
+        end_reason_plots[exp['end_reason']] = []
+    end_reason_plots[exp['end_reason']].append((i + 1, exp['sim_time']))
+
+    # Intermediate plotting and printing for specific intervals
+    if (i % PRINT_EVAL_STEPS == 0) or (i == len(experiments) - 1):
+        print(f"EXPERIMENT {i + 1}")
         preds = exp["PREDATORS"]
         preys = exp["PREYS"]
         print(f"num_predators: {len(preds)}, num_preys: {len(preys)}")
         print(f"sim_time: {exp['sim_time']}")
         print(f"end_reason: {exp['end_reason']}")
-
-        unique_end_reasons.add(exp['end_reason'])
-
-        if exp['end_reason'] in end_reason_counts:
-            end_reason_counts[exp['end_reason']] += 1
-        else:
-            end_reason_counts[exp['end_reason']] = 1
-        if exp['end_reason'] not in end_reason_plots:
-            end_reason_plots[exp['end_reason']] = []
-        end_reason_plots[exp['end_reason']].append((i+1, exp['sim_time']))
 
         num_plots = 10 
         fig, axs = plt.subplots(num_plots // 2, 2, figsize=(15, 2 * num_plots))
@@ -209,10 +212,12 @@ end_reason_marker = {reason: markers[i % len(markers)] for i, reason in enumerat
 plt.figure(figsize=(10, 6))
 for reason, data in end_reason_plots.items():
     x, y = zip(*data)
+    y = [round(float(y_i), 2) for y_i in y]
     plt.scatter(x, y, marker=end_reason_marker[reason], label=reason)
 
 plt.title("End Reasons for Each Experiment")
 plt.xlabel("Experiment Number")
 plt.ylabel("Simulation Time")
 plt.legend()
+
 plt.show()
